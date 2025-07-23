2ad16df0931a3f52420cc5925b005579b7e180fd
Claubuck <camposclaudio@live.com>
2025-07-23 14:46:01 -0300

    fix traduction

---
+++ lang/es/datatables.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'sEmptyTable'     => 'No hay datos disponibles en la tabla',
+    'sInfo'           => 'Mostrando _START_ a _END_ de _TOTAL_ entradas',
+    'sInfoEmpty'      => 'Mostrando 0 a 0 de 0 entradas',
+    'sInfoFiltered'   => '(filtrado de _MAX_ entradas totales)',
+    'sInfoPostFix'    => '',
+    'sInfoThousands'  => ',',
+    'sLengthMenu'     => '_MENU_ registros por página',
+    'sLoadingRecords' => 'Cargando...',
+    'sProcessing'     => 'Procesando...',
+    'sSearch'         => 'Buscar:',
+    'sZeroRecords'    => 'No se encontraron registros coincidentes',
+    'oPaginate'       => [
+        'sFirst'    => 'Primero',
+        'sLast'     => 'Último',
+        'sNext'     => 'Siguiente',
+        'sPrevious' => 'Anterior',
+    ],
+    'oAria' => [
+        'sSortAscending'  => ': activar para ordenar la columna de forma ascendente',
+        'sSortDescending' => ': activar para ordenar la columna de forma descendente',
+    ],
+]; 
