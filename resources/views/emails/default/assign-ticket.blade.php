676bfaf5c87be0f5d7b892333317e372d0574ce9
Claubuck <camposclaudio@live.com>
2026-02-03 01:12:43 -0300

    Add Spanish translations for ticket statuses and email notifications in DatabaseSeeder and email templates.

--- resources/views/emails/default/assign-ticket.blade.php
+++ resources/views/emails/default/assign-ticket.blade.php
@@ -1,1 +1,1 @@
-<div>Hello {!!$ticket_agent_name!!},<br><br>Ticket No: {!!$ticket_number!!}<br>Has been assigned to you by {!!$ticket_assigner!!}&nbsp;<br><br>Thank You<br>Kind Regards,<br>{!!$system_from!!}</div>
+<div>Hello {!!$ticket_agent_name!!},<br><br>Ticket No: {!!$ticket_number!!}<br>fue asignado a usted por {!!$ticket_assigner!!}&nbsp;<br><br>Thank You<br>Kind Regards,<br>{!!$system_from!!}</div>
